1e5793fb9950a6492c939de06f51b2bb729f5ca3
AlexeyNoskov <alexeinoskov91@gmail.com>
2020-09-06 20:42:11 +0300

    Initial commit of the project v.1.04

--- movies/admin.py
+++ movies/admin.py
@@ -2,16 +2,15 @@ from django import forms
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
-# from modeltranslation.admin import TranslationAdmin
 
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
 
 class MovieAdminForm(forms.ModelForm):
     """Форма с виджетом ckeditor"""
     description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
-    # description_en = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
 
-    class Meta: 
+    class Meta:
         model = Movie
         fields = '__all__'
 
@@ -36,7 +35,7 @@ class MovieShotsInline(admin.TabularInline):
     readonly_fields = ("get_image",)
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="100" height="60"')
+        return mark_safe(f'<img src={obj.image.url} width="100" height="110"')
 
     get_image.short_description = "Изображение"
 
@@ -51,6 +50,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    actions = ["publish", "unpublish"]
     form = MovieAdminForm
     readonly_fields = ("get_image",)
     fieldsets = (
@@ -78,12 +78,36 @@ class MovieAdmin(admin.ModelAdmin):
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
 
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    publish.short_description = "Опубликовать"
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = "Снять с публикации"
+    unpublish.allowed_permissions = ('change',)
+
     get_image.short_description = "Постер"
 
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
-    """Отзывы"""
+    """Отзывы к фильму"""
     list_display = ("name", "email", "parent", "movie", "id")
     readonly_fields = ("name", "email")
 
@@ -101,7 +125,7 @@ class ActorAdmin(admin.ModelAdmin):
     readonly_fields = ("get_image",)
 
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="50" height="70"')
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
 
     get_image.short_description = "Изображение"
 
--- movies/views.py
+++ movies/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie
+from .models import Movie, Category, Actor
 from .forms import ReviewForm
+
 
 class MoviesView(ListView):
     """Список фильмов"""
-    # template_name = 'movies.html' это тестил почему рвет templates. Зато выяснил. Из-за неявного задания шаблоны берутся из папки movie. Вьюха по статьям template_name задает явно, поэтому они ищутся сразу в корне templates
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    # template_name явно не указал, потому что джанго сам ищет файл "(имя_модели)_list.html" - название по умолчанию
+
 
 class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
-    # template_name не указал, потому что джаного сам ищет файл "(имя_модели)_detail.html" - название по умолчанию
-    
+
+
 class AddReview(View):
     """Отзывы"""
     def post(self, request, pk):
@@ -30,4 +30,9 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
-                
+        
+class ActorView(DetailView):
+    """Вывод информации о актере"""
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = "name"
